85421eb1717e9745c012fbcd47d40cdf39f4eae3
Leon4ever2802 <94672224+Leon4ever2802@users.noreply.github.com>
2026-06-22 13:23:48 +0200

    fix: update version in openapi.yaml correctly (#11)

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 
-const packageJSON = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const packageJSON = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const packageVersion = packageJSON.version;
 
-const openapiSpecPath = './src/openapi.yaml';
+const openapiSpecPath = 'src/openapi.yaml';
 const openapiSpecContent = fs.readFileSync(openapiSpecPath, 'utf8');
-openapiSpecContent.replace(/^version:.*$/m, `version: ${packageVersion}`);
 fs.writeFileSync(
   openapiSpecPath,
   openapiSpecContent.replace(
-    /^version:.*$/m,
+    /version:.*$/m,
     `version: ${packageVersion}`
   )
 );
